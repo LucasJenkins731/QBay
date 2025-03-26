e9491c5cb2e6a7fbbb53cde3c71cb999367e41c1
reZdotjson <0083rei@proton.me>
2025-03-26 19:21:05 -0400

    added Sell

--- Sell.java
+++ Sell.java
@@ -1,6 +1,6 @@
 public class Sell {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("FW Laptop 13, Textbook, Switch, QU Pen Set, LTT Backpack");
     }    
 }
