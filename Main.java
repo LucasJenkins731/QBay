7618de70553c2291177fad06211fef3e312c4cbf
isabeleila <leilaiayers@gmail.com>
2025-04-01 15:27:40 -0400

    added login to main

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        Login loguser = new Login();
+        
+        Logout logoutfunction = new Logout();
 
-        System.out.println("Hello World");
+
     }    
 }
