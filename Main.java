ed442a6cd64d123dda0d39885d564d280312fc6a
reZdotjson <0083rei@proton.me>
2025-04-01 19:03:56 -0400

    Made main menu in main class

--- Main.java
+++ Main.java
@@ -6,8 +6,17 @@ public class Main {
 
         System.out.println("What would you like to do, Buy, Sell, Cart, or Logout?");
         String in = input.nextLine().toUpperCase();
+        if ("BUY".equals(in)) {
+            //Buy.main(null);
+        }
         if ("SELL".equals(in)) {
             Sell.main(null);
+        }
+        if ("CART".equals(in)) {
+            //Cart.main(null);
+        }
+        if ("LOGOUT".equals(in)) {
+            //Logout.main(null);
         }
     }
 }
