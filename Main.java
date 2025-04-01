3ac70b6ade321ef4b561141449f2dcd7bcc66eb6
LucasJenkins731 <lgjenkins@quinnipiac.edu>
2025-04-01 19:33:42 -0400

    Keeps track of items that are added to cart with an array

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        buy buy = new buy();
-        buy.buy();
+        
     }    
 }
--- buy.java
+++ buy.java
@@ -1,42 +1,44 @@
 import java.util.*;
 
 public class buy {
-    
+
+    private static ArrayList<String> cart = new ArrayList<>();
+
     public static void buy(){
             Scanner scanner = new Scanner(System.in);
     
             //Prompts user with items
             System.out.println("\nWhat would you like to buy? (If you would like more info on an item type info)");
-            System.out.println("1: Laptop\n2: Textbook\n3: Switch\n4: Backpack\n5: Pens\n6: Go back");
+            System.out.println("1: Framework Laptop 16\n2: Math Textbook\n3: Nintendo Switch OLED Model\n4: QU Branded Pens\n5: LTT Computer Backpack\n6: Go back");
             String buy = scanner.nextLine();
     
             switch (buy) {
                 case "info":
                     //if info is entered, prompts user to enter number of item they want more info on
                     System.out.println("\nWhich item would you like more info on?");
-                    System.out.println("1: Laptop\n2: Textbook\n3: Switch\n4: Backpack\n5: Pens\n6: Go back");
+                    System.out.println("1: Framework Laptop 16\n2: Math Textbook\n3: Nintendo Switch OLED Model\n4: QU Branded Pens\n5: LTT Computer Backpack\n6: Go back");
                     String info = scanner.nextLine();
 
                     //Entered number gives corresponding info
                     switch (info) {
                         case "1":
-                            System.out.println("\nItem: Laptop\nCategory: Electronics\nSeller: Lucas Jenkins lucas.jenkins@quinnipiac.edu\nPrice: $1000.00");
+                            System.out.println("\nItem: Framework Laptop 16\nCategory: Tech\nSeller: Lucas Jenkins lucas.jenkins@quinnipiac.edu\nPrice: $1600.00");
                             buy();
                             break;
                         case "2":
-                            System.out.println("\nItem: Textbook\nCategory: Books\nSeller: Isabela Ayers  leila.ayers@quinnipiac.edu\nPrice: $20.00");
+                            System.out.println("\nItem: Math Textbook\nCategory: Books\nSeller: Isabela Ayers  leila.ayers@quinnipiac.edu\nPrice: $200.00");
                             buy();
                             break;
                         case "3":
-                            System.out.println("\nItem: Switch\nCategory: Electronics\nSeller: Daniel Tracz daniel.tracz@quinnipiac.edu\nPrice: $300.00");
+                            System.out.println("\nItem: Nintendo Switch OLED Model\nCategory: Tech\nSeller: Lucas Jenkins lucas.jenkins@quinnipiac.edu\nPrice: $340.00");
                             buy();
                             break;
                         case "4":
-                            System.out.println("\nItem: Backpack\nCategory: Clothing\nSeller: Kate Francis kate.francis@quinnipiac.edu\nPrice: $25.00");
+                            System.out.println("\nItem: QU Branded Pens\nCategory: Apparel\nSeller: Kate Francis kate.francis@quinnipiac.edu\nPrice: $4.00");
                             buy();
                             break;
                         case "5":
-                            System.out.println("\nItem: Pens\nCategory: Writing utensils\nSeller: Lucas Jenkins lgjenkins@quinnipiac.edu\nPrice: $10.00");
+                            System.out.println("\nItem: LTT Computer Backpack\nCategory: Apparel\nSeller: Daniel Tracz daniel.tracz@quinnipiac.edu\nPrice: $50.00");
                         case "6":
                             buy();
                             break;
@@ -45,19 +47,24 @@ public class buy {
                     break;
                 //If number is entered, corresponding item is added to cart
                 case "1":
-                    System.out.println("Laptop has been added to cart");
+                    System.out.println("Framework Laptop 16 has been added to cart");
+                    cart.add("Framework Laptop 16");
                     break;
                 case "2":
-                    System.out.println("Textbook has been added to cart");
+                    System.out.println("Math Textbook has been added to cart");
+                    cart.add("Math Textbook");
                     break;
                 case "3":
-                    System.out.println("Switch has been added to cart");
+                    System.out.println("Nintendo Switch OLED Model has been added to cart");
+                    cart.add("Nintendo Switch OLED Model");
                     break;
                 case "4":
-                    System.out.println("Backpack has been added to cart");
+                    System.out.println("QU Branded Pens has been added to cart");
+                    cart.add("QU Branded Pens");
                     break;
                 case "5":
-                    System.out.println("Pens have been added to cart");
+                    System.out.println("LTT Computer Backpack have been added to cart");
+                    cart.add("LTT Computer Backpack");
                     break;
                 case "6":
                     System.out.println("Back to menu");
@@ -65,6 +72,7 @@ public class buy {
                     break;
                 default:
                     System.out.println("Invalid input");
+                    buy();
                     break;
             }
         } 
