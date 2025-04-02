39407ff00d70702ac094ceec155518934802e189
isabeleila <leilaiayers@gmail.com>
2025-04-02 09:05:52 -0400

    Logout will close out program and then redirect back to login for them to select the exit option

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Login.main(null);
+
         Scanner input = new Scanner(System.in);
 
         System.out.println("What would you like to do, Buy, Sell, Cart, or Logout?");
